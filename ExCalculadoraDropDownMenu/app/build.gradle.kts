1229b76f5f964635190f400d20c9e373d3879566
sjo <omar.idris.7e7@itb.cat>
2023-11-28 12:17:45 +0100

    awddaw

--- ExCalculadoraDropDownMenu/app/build.gradle.kts
+++ ExCalculadoraDropDownMenu/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.excalculadoradropdownmenu"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.excalculadoradropdownmenu"
--- ExCalculadoraDropDownMenu/app/src/main/java/com/example/excalculadoradropdownmenu/MainActivity.kt
+++ ExCalculadoraDropDownMenu/app/src/main/java/com/example/excalculadoradropdownmenu/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.example.excalculadoradropdownmenu
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.excalculadoradropdownmenu.ui.theme.ExCalculadoraDropDownMenuTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +48,91 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Body()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Body() {
+    // declara variables
+    var num1 by remember { mutableStateOf("") }
+    var num2 by remember { mutableStateOf("") }
+    var show by rememberSaveable { mutableStateOf(false) }
+    var showToast by rememberSaveable { mutableStateOf(false) }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(24.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        Text(text = "Calculadora\n", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+        TextField(value = num1, onValueChange = { num1 = it }, label = { Text("Num 1") })
+        TextField(value = num2, onValueChange = { num2 = it }, label = { Text("Num 2") })
+
+        Text(text = "", fontSize = 20.sp)
+        val op = myDropDownMenu()
+        Text(text = "", fontSize = 20.sp)
+
+        Button(onClick = { show = true }) {
+            Text(text = "Calcular")
+        }
+
+        if (show) {
+            var result = 0
+            if (op == "División" && num2 == "0") {
+                Toast.makeText(LocalContext.current, "No se puede dividir entre 0 bobo", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Text (text = "Resultado: $result", fontSize = 18.sp, fontWeight = FontWeight.Bold, color = Color.Green)
+                when (op) {
+                    "Suma" -> result = num1.toInt() + num2.toInt()
+                    "Resta" -> result = num1.toInt() - num2.toInt()
+                    "Multiplicación" -> result = num1.toInt() * num2.toInt()
+                    "División" -> result = num1.toInt() / num2.toInt()
+                }
+            }
+
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun myDropDownMenu(): String {
+    var operacion by remember { mutableStateOf("") }
+    var expanded by remember { mutableStateOf(false) }
+    val options = listOf("Suma", "Resta", "Multiplicación", "División")
+
+    OutlinedTextField(
+        value = operacion,
+        onValueChange = { operacion = it },
+        enabled = false,
+        readOnly = true,
+        modifier = Modifier
+            .clickable { expanded = true }
+            .width(200.dp)
+    )
+
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { expanded = false },
+    ) {
+        options.forEach {
+            DropdownMenuItem(text = { Text(text = it) }, onClick = {
+                expanded = false
+                operacion = it
+            })
+        }
+    }
+    return operacion
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ExCalculadoraDropDownMenuTheme {
-        Greeting("Android")
+        Body()
     }
 }
